5ea030c86a866e520ddc7ab176a99eeec22f78fc
CS Lean GPT Server <__leangpt@leangpt.cs.uchicago.edu>
2024-01-08 06:16:39 -0600

    Go back to the older version and 4.2 lean

--- lakefile.lean
+++ lakefile.lean
@@ -3,14 +3,9 @@ open Lake DSL
 
 package «ts-leangpt4» where
   -- Settings applied to both builds and interactive editing
-  leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
-    ⟨`pp.proofs.withType, false⟩
-  ]
-  -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git"@"3ce43c18f614b76e161f911b75a3e1ef64162"
 
 @[default_target]
 lean_lib «TsLeangpt4» where
