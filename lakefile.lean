aff0288f7119c9ae6c8521c270d65d856c75d686
CS Lean GPT Server <__leangpt@leangpt.cs.uchicago.edu>
2024-01-09 06:26:25 -0600

    Use local mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -4,8 +4,7 @@ open Lake DSL
 package «ts-leangpt4» where
   -- Settings applied to both builds and interactive editing
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"@"3ce43c18f614b76e161f911b75a3e1ef64162"
+require mathlib from "~/.cache/lean_dojo/leanprover-community-mathlib4-3ce43c18f614b76e161f911b75a3e1ef641620ff/mathlib4/"
 
 @[default_target]
 lean_lib «TsLeangpt4» where
